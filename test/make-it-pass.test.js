037f6013c7d57dde10f2ed8b34878afb7f94d937
elie <zerbib16@gmail.com>
2018-06-03 01:55:27 +0300

    GREEN: a new correct test that i wrote

--- test/make-it-pass.test.js
+++ test/make-it-pass.test.js
@@ -4,7 +4,16 @@ function sum (a, b) {
   return a+b;
 }
 
+function digit (a) {
+  return a%10;
+}
+
 test('sum should return the addition of two numbers', function (t) {
   t.equal(3, sum(1, 2)); // make this test pass by completing the add function!
+  t.end();
+});
+
+test('digit should return the last digit of a number', function (t) {
+  t.equal(3, digit(13));
   t.end();
 });
